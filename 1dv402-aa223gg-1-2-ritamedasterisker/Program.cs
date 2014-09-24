64a27f59b74b4b588df1d3828d0dde7255a19509
Anna Aldenmark <aa223gg@student.lnu.se>
2014-09-24 11:08:03 +0200

    commit

---
+++ 1dv402-aa223gg-1-2-ritamedasterisker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402_aa223gg_1_2_ritamedasterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int row = 1; row <= 25; row++)
+            {
+                switch (row % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                }
+                if (row % 2 == 0)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int col = 1; col <= 39; col++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ 1dv402-aa223gg-1-2-ritamedasterisker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("1dv402-aa223gg-1-2-ritamedasterisker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("1dv402-aa223gg-1-2-ritamedasterisker")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e4ad77f-a16c-45bf-b580-5c65f90f40ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
